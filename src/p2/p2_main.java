a4b3125546e0ef49b3f110642ad4299382a81d41
Huhnsohn <leon.kolij@web.de>
2025-10-30 18:07:52 +0100

    Präsenzaufgabe 2 und 3 hinzugefügt

---
+++ src/p2/p2_main.java
@@ -0,0 +1,14 @@
+package p2;
+
+public class p2_main {
+	public static void main(String[] args) {
+		int i = 1;
+		boolean b = false;
+		
+		if (i > 10) {
+		b = true; 
+		}		
+		System.out.println("i = " + i + ", b = " + b);
+	}
+
+}
